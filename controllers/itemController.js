3ff155519a40bcd4c47b27b0f86dd0458232617f
bawantha.w <bawantha@sentientnow.com>
2023-06-07 09:12:27 +0530

    adding few comments

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,7 +1,9 @@
 const Item = require('../models/item');
 
+// to get all data from the item table
 async function getAll(req, res, next) {
     try {
+        // fetching data from the item table
         const allItems = await Item.find({}, { _id: 0 });
 
         res.status(200).send(allItems);
